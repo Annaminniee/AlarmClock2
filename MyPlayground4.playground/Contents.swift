600c1b011f204778a8aa61d7d0bdad98bf22309b
tomokazutakahashi4 <takahashitomokazu4@gmail.com>
2023-07-17 18:00:23 +0900

    ああああ

--- MyPlayground4.playground/Contents.swift
+++ MyPlayground4.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+class AlarmClock {
+    var timer: Timer?
+    var count = 0
+    var limit = 0
+    
+    func start() {
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(countup),
+            userInfo: nil,
+            repeats: true)
+    }
+    
+    @objc func countup() {
+        count += 1
+        let hour = count / 60
+        let min = count % 60
+        print("現在\(hour)時\(min)分です。")
+        if limit == count {
+            print("朝です。起きてください！現在\(hour)時\(min)分です。")
+            timer?.invalidate()
+        }
+    }
+}
+let alarmClock = AlarmClock()
+let setLimit = 120
+alarmClock.limit = setLimit
+print("起床時間は\(setLimit / 60)時に設定します。")
+alarmClock.start()
+print("アラームをセットします。")
