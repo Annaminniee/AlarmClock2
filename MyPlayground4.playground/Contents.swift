8bff45425aedb7c83fc5dac975a07c30362dffb8
Anna <anna.kikuchihara@gmail.com>
2023-07-19 13:50:13 +0900

    指摘修正箇所を変更しました

--- MyPlayground4.playground/Contents.swift
+++ MyPlayground4.playground/Contents.swift
@@ -21,21 +21,23 @@ class AlarmClock {
         count += 1
         let hour = count / 60
         let min = count % 60
-        print("現在\(hour)時\(min)分です。")
-        if limit == count {
-            print("朝です。起きてください！現在\(hour)時\(min)分です。")
+        if setLimit == 0 {
+            print("起床時間が設定されていません！")
             timer?.invalidate()
+        } else if limit == count {
+            print("朝です。起きてください！現在\(hour)時\(min)分です。")
+           
+            timer?.invalidate()
+        } else {
+            print("現在\(hour)時\(min)分です。")
         }
     }
 }
 let alarmClock = AlarmClock()
-let setLimit = 0
+
+let setLimit = 120
 alarmClock.limit = setLimit
-print("アラームをセットします。")
-let setTime = true
-if setLimit == nil {
-    print("起床時間は\(setLimit / 60)時に設定します。")
-} else if setLimit == 0 {
-    print ("起床時間が設定されていません！")
-}
+print("起床時間は\(setLimit / 60)時に設定します。")
+
 alarmClock.start()
+print("アラームをセットします。")
